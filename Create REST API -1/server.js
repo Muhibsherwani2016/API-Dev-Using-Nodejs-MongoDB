d21b0989128b504e7fccdd8979071ff3405b16d0
Muhibsherwani2016 <35762980+Muhibsherwani2016@users.noreply.github.com>
2019-08-23 11:47:12 +0500

    basic api develop

--- Create REST API -1/server.js
+++ Create REST API -1/server.js
@@ -6,4 +6,19 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
+// Router 1
+app.use("/posts", (req, res) => {
+  res.send({
+    ok:true
+  })
+});
+
+
+// Router 2
+app.use("/test", (req, res) => {
+  res.send({
+    ok:true
+  })
+});
+
 server.listen(port);
