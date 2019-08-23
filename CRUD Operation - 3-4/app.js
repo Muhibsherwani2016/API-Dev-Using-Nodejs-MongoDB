87c45e90f8677486ca7f63c22b967ce2d64bf978
Muhibsherwani2016 <35762980+Muhibsherwani2016@users.noreply.github.com>
2019-08-23 11:53:49 +0500

    crud file

---
+++ CRUD Operation - 3-4/app.js
@@ -0,0 +1,83 @@
+const express = require('express');
+
+const app = express();
+
+const bodyParser = require('body-parser');
+
+const morgan = require("morgan");
+
+// Database Connection
+require("./mongo");
+
+// Models
+require("./model/Post");
+
+//Middleware
+app.use(bodyParser.json())
+    .use(morgan())
+
+// READ BY ID
+app.get("/:postId", async (req, res) => {
+    try{
+        const post = await Post.findOne({_id: req.params.postId});
+        res.send(post);
+    }catch(error){
+        res.status(500);
+    }
+});
+
+
+// READ
+app.get("/", async (req, res) => {
+    try{
+        const posts = await Post.find({})
+        res.send(posts);
+    }catch(error){
+        res.status(500);
+    }
+});
+
+
+// CREATE
+app.post("/", async (req, res) => {
+    try{
+        const post = new Post();
+        post.title = req.body.title;
+        post.content = req.body.content;
+        await post.save();
+        res.send(post);
+    }catch(error){
+        res.status(500);
+    }
+});
+
+// UPDATE
+app.put("/:postId", async (req, res) => {
+    try{
+        const post = await Post.findByIdAndUpdate({
+            _id: req.params.postId
+        }, req.body,{
+           new: true,
+           runValidators: true
+        });
+
+        res.send(post);
+    }catch(error){
+        res.send(500);
+    }
+});
+
+// DELETE
+app.delete("/:postId", async (req, res) => {
+    try{
+        const post = await Post.findByIdAndRemove({
+            _id: req.params.postId
+        });
+
+        res.send(post);
+    }catch(error){
+        res.send(500);
+    }
+});
+
+module.exports = app;
