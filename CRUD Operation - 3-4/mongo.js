68eae97480d49629e0787af77c0e315780450064
Muhibsherwani2016 <35762980+Muhibsherwani2016@users.noreply.github.com>
2019-08-23 11:54:38 +0500

    part 3 -4

---
+++ CRUD Operation - 3-4/mongo.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.Promise = global.Promise;
+console.log(process.env.MONGOURI);
+
+mongoose.connect(process.env.MONGOURI);
+
